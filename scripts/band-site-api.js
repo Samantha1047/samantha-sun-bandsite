35433b6e9baee8c61223d5797660079f9139d06f
Samantha Sun <samisun1047@gmail.com>
2024-05-31 20:22:11 -0700

    add code for api.js, add axios in index.html

---
+++ scripts/band-site-api.js
@@ -0,0 +1,37 @@
+/* {
+    "api_key": "fcb695a7-499c-4df9-a259-10354602d489"
+} */
+
+class BandSiteApi {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
+  }
+
+  async postComment(comment) {
+    try {
+      const postComment = await axios.post(`${this.baseUrl}/comments?api_key=${this.apiKey}`, comment);
+      return postComment.data;
+    } catch (err) {
+      console.error(`Failed to post comment: ${err}`);
+    }
+  }
+
+  async getComments() {
+    try {
+      const commentsData = await axios.get(`${this.baseUrl}/comments?api_key=${this.apiKey}`);
+      return commentsData.data.sort((a, b) => b.timestamp - a.timestamp);
+    } catch (err) {
+      console.error(`Failed to get comments: ${err}`);
+    }
+  }
+
+  async getShows() {
+    try {
+      const showData = await axios.get(`${this.baseUrl}/showdates?api_key=${this.apiKey}`);
+      return showData.data;
+    } catch (err) {
+      console.error(`Failed to get shows: ${err}`);
+    }
+  }
+}
